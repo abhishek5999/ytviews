cbf770fb1876541c49a42ba133d99e18cfc25770
abhishek5999 <31465399+abhishek5999@users.noreply.github.com>
2020-06-12 12:29:38 +0530

    Add files via upload

--- ytviews.py
+++ ytviews.py
@@ -7,15 +7,16 @@ browser1 = webdriver.Chrome()
 browser2 = webdriver.Chrome()
 browser3 = webdriver.Chrome()
 
-
+# put video link in ''
 for i in range(views):
-    browser1.get('https://youtu.be/SyaU5YJ851o')
+    browser1.get('')
     time.sleep(view_time)
-    browser2.get('https://youtu.be/SyaU5YJ851o')
+    browser2.get('')
     time.sleep(view_time)
-    browser3.get('https://youtu.be/SyaU5YJ851o')
+    browser3.get('')
     time.sleep(view_time)
 
 browser1.close()
 browser2.close()
 browser3.close()
+# script by abhishek5999
